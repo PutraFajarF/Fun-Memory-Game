650f8992b3f17b8d692098f9a2b117a5e366aadb
Putra Fajar <putrautalls@gmail.com>
2022-06-03 13:27:54 +0700

    added matched Property to Cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false  },
+  { "src": "/img/potion-1.png", matched: false  },
+  { "src": "/img/ring-1.png", matched: false  },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false }
 ]
 
 function App() {
@@ -37,14 +37,22 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log('Those card match');
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card;
+            }
+          })
+        })
         resetTurn()
       } else {
-        console.log('Not match, please try again');
         resetTurn()
       }
     }
   }, [choiceOne, choiceTwo])
+  console.log(cards);
 
   // reset choices & increase turn
   const resetTurn = () => {
